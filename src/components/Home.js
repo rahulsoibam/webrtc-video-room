33d1267d4ea6ebb4965e421c1343c25a77421998
Rahul Soibam <rahulsoibam@gmail.com>
2018-11-22 03:02:17 +0530

    Home complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const Home = props =>
     <div>
       <h1 itemProp="headline">Koubru First Demo</h1>
       <p>Please specify a topic to start the debate.</p>
-      <input type="text" name="room" value={ props.roomId } onChange={props.handleChange} pattern="^\w+$" maxLength="255" required autoFocus title="Room name should only contain letters or numbers."/>
+      <input type="text" name="room" placeholder="Type topic here..." onChange={props.handleChange} pattern="^\w+$" maxLength="255" required autoFocus title="Room name should only contain letters or numbers."/>
       <Link className="primary-button" to={ '/r/' + props.roomId }>Create/Join Topic</Link>
       { props.rooms.length !== 0 && <div>Recently created topics:</div> }
       { props.rooms.map(room => <Link key={room} className="recent-room" to={ '/r/' + room }>{ room }</Link>) }
